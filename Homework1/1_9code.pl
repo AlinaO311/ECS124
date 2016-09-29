a41a50b02fcb1e3e2b2d4dcee9af13786bd0b824
AlinaO311 <alina.orozco12@gmail.com>
2016-09-28 22:14:53 -0700

    Final exercise of PartA

---
+++ Homework1/1_9code.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+#
+use strict; #
+use warnings;
+
+sub main
+{
+
+    print "dna sequence: "; ##prompt user for input
+    my $DNA_seq = <STDIN> ; #makes local variable for input
+    chomp $DNA_seq; #removes any newline or space characters from end of string input
+    exit 0 if ($DNA_seq eq ""); #if the user input string is blank, exit function
+
+    $DNA_seq=~ tr/ACGTacgt//cd; #remove all characters that do not match charcters specified in search
+    my $purines= $DNA_seq =~ tr/AGag/AGag/; #count total number of purines in DNA_seq string.
+
+    print "There are $purines purines in this sequence \n";
+}
+
+main();
